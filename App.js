3d88d8f9415d3ad4a7fe3a16d5204a22741ad55f
JB <drv98@naver.com>
2024-02-06 11:48:15 +0900

    토큰 암호화저장

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { useCallback } from 'react';
 import LoginScreen from './App/Screen/LoginScreen';
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { SignedIn, SignedOut } from '@clerk/clerk-react';
+import * as SecureStore from 'expo-secure-store';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -26,8 +27,30 @@ export default function App() {
     return null;
   }
 
+  const tokenCache = {
+    //jwt토큰을 받아서 암호화함
+    async getToken(key) {
+      try {
+        return SecureStore.getItemAsync(key);
+      } catch (err) {
+        return null;
+      }
+    },
+    //암호화된 토큰을 저장함
+    async saveToken(key, value) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (err) {
+        return;
+      }
+    },
+  };
+
   return (
-    <ClerkProvider publishableKey="pk_test_YWxlcnQtbXVsZS0zNi5jbGVyay5hY2NvdW50cy5kZXYk">
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey="pk_test_YWxlcnQtbXVsZS0zNi5jbGVyay5hY2NvdW50cy5kZXYk"
+    >
       <View style={styles.container} onLayout={onLayoutRootView}>
         <SignedIn>
           <Text>로그인 되었음.</Text>
