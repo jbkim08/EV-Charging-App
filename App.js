17a304beb87db611c62f0e51f01e28073f317bb8
JB <drv98@naver.com>
2024-02-06 09:47:00 +0900

    폰트및 스플래쉬 스크린 적용

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Pre-Regular': require('./assets/fonts/Pretendard-Regular.otf'),
+    'Pre-Medium': require('./assets/fonts/Pretendard-Medium.otf'),
+    Jalnan: require('./assets/fonts/Jalnan2.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>헬로우 월드!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontSize: 30 }}>헬로우 월드!</Text>
+      <Text style={{ fontFamily: 'Pre-Regular', fontSize: 30 }}>헬로우 월드!</Text>
+      <Text style={{ fontFamily: 'Jalnan', fontSize: 30 }}>여행가고 싶다!</Text>
       <StatusBar style="auto" />
     </View>
   );
