cd08df3aba5dd71610f3eaa0c1467e4a148bd744
JB <drv98@naver.com>
2024-02-13 09:41:22 +0900

    구글맵 표시

---
+++ App/Screen/AppMapView.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default function AppMapView() {
+  return (
+    <View>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
--- App/Screen/HomeScreen.jsx
+++ App/Screen/HomeScreen.jsx
@@ -1,10 +1,11 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
+import AppMapView from './AppMapView';
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>HomeScreen</Text>
+    <View>
+      <AppMapView />
     </View>
   );
 }
