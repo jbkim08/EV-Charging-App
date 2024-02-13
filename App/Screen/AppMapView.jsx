5b6776ed0c50f99f19a39847209503cb7f87c5a9
JB <drv98@naver.com>
2024-02-13 10:32:39 +0900

    구글맵에 현재위치 적용

--- App/Screen/AppMapView.jsx
+++ App/Screen/AppMapView.jsx
@@ -1,8 +1,11 @@
 import { StyleSheet, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import MapStyle from '../Utils/MapViewStyle.json';
+import { useContext } from 'react';
+import { UserLocationContext } from '../Context/UserLocationContext';
 
 export default function AppMapView() {
+  const { location, setLocation } = useContext(UserLocationContext);
   return (
     <View>
       <MapView
@@ -10,6 +13,12 @@ export default function AppMapView() {
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
         customMapStyle={MapStyle}
+        region={{
+          latitude: location?.latitude,
+          longitude: location?.longitude,
+          latitudeDelta: 0.0422,
+          longitudeDelta: 0.0421,
+        }}
       />
     </View>
   );
